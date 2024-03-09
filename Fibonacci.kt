486bf41e4edb75e925da03cfcf649269df13fd17
Joao Ávila <jolucas245@gmail.com>
2024-03-09 18:24:41 -0300

    :tada:feat: Solution

---
+++ Fibonacci.kt
@@ -0,0 +1,21 @@
+fun main() {
+    var n = 21 // Coloque nesta linha o valor que deseja verificar se está na sequência de Fibonacci
+    var mensagem = when {
+        estaEmFibonacci(n) -> "O número $n ESTÁ na sequência de Fibonacci"
+        else -> {
+            "O número $n NÃO ESTÁ na sequência de Fibonacci"
+        }
+    }
+    println(mensagem)
+}
+
+fun estaEmFibonacci(n: Int): Boolean{
+    var a = 0
+    var b = 1
+    while(b < n){
+        var temp = b
+        b += a
+        a = temp
+    }
+    return b == n
+}
